7d7bd108447927e2597b0f18f4235fe4de89945e
pmbadger <pm@steepshot.io>
2024-08-10 10:01:48 +0400

    add 'selected' field to Product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, SelectedProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    selected = serializers.SerializerMethodField()
     class Meta:
         model = Product
-        fields = ["id", "name", "description", "price", "stock"]
+        fields = ["id", "name", "description", "price", "stock", "selected"]
+
+    def get_selected(self, obj):
+        request = self.context.get('request')
+        user = request.user
+        if user.is_authenticated:
+            return SelectedProduct.objects.filter(user=user, product=obj).exists()
+        else:
+            return False
 
 
 class ProductSelectSerializer(serializers.Serializer):
